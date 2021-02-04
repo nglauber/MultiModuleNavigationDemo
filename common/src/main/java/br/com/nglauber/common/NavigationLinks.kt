d1d325ba0256a1de22336d591a25e9530913d6d6
Nelson Glauber <nglaubervasc@gmail.com>
2021-02-04 18:27:12 -0300

    Moving ids and deep link to common module

---
+++ common/src/main/java/br/com/nglauber/common/NavigationLinks.kt
@@ -0,0 +1,7 @@
+package br.com.nglauber.common
+
+import android.net.Uri
+
+object NavigationLinks {
+    val LINK_PASSWORD_CHANGE = Uri.parse("ngvl://passwordupdate")
+}
--- feature_onboarding/src/main/java/br/com/nglauber/feature_onboarding/HomeFragment.kt
+++ feature_onboarding/src/main/java/br/com/nglauber/feature_onboarding/HomeFragment.kt
@@ -1,12 +1,12 @@
 package br.com.nglauber.feature_onboarding
 
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import br.com.nglauber.common.NavigationLinks
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
@@ -14,11 +14,10 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
         view.run {
             findViewById<Button>(R.id.btnProfile).setOnClickListener {
-                // from onboarding module...
                 findNavController().navigate(R.id.action_profile, bundleOf("user" to br.com.nglauber.common.User(1, "Nelson")))
             }
             findViewById<Button>(R.id.btnPassword).setOnClickListener {
-                findNavController().navigate(Uri.parse("ngvl://passwordupdate"))
+                findNavController().navigate(NavigationLinks.LINK_PASSWORD_CHANGE)
             }
         }
     }
